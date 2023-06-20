7362f62e8785e6eb52024487f35bcd191195e91f
Katana Nevermore <grenkachannel@hotmail.com>
2023-06-20 08:57:52 +0300

    refactoring

--- Pseudorandom Bit Sequence Generator/main.py
+++ Pseudorandom Bit Sequence Generator/main.py
@@ -1,30 +1,35 @@
 import psutil
 import sympy
 import time
-f = open('C:/Users/Grenk/OneDrive/Рабочий стол/test.txt', 'w')
+f = open('PBSG.txt', 'w')
 
+MIN = 2 ** 127
+MAX = 2 ** 128 - 1
+COUNT = 1500000
 
-def prime_number():
-    prime_number = sympy.randprime(2**127, 2**128-1)
+def create_prime_number():
+    prime_number = sympy.randprime(MIN, MAX)
     result = prime_number * 4 + 3
     return result
 
 
-startTime = time.time()
-p = prime_number()
-q = prime_number()
-M = p * q
-print('----------------------------------------------------Числа блюма--------------------------------------------')
-print('p = ', p)
-print('q = ', q)
-print('-----------------------------------------------------------------------------------------------------------')
-seed = psutil.cpu_stats()[0]
+def main():
+    startTime = time.time()
+    p = create_prime_number()
+    q = create_prime_number()
+    M = p * q
+    print('----------------------------------------------------Числа блюма--------------------------------------------')
+    print('p = ', p)
+    print('q = ', q)
+    print('-----------------------------------------------------------------------------------------------------------')
+    seed = psutil.cpu_stats()[0]
 
+    for i in range(COUNT):
+        seed = pow(seed, 2) % M
+        bit = str(seed % 2)
+        f.write(bit)
+    f.close()
+    print(f"Completed in {time.time() - startTime} seconds.")
 
-for i in range(0, 1500000):
-    seed = pow(seed, 2) % M
-    bit = str(seed % 2)
-    f.write(bit)
-f.close()
-print(f"Completed in {time.time() - startTime} seconds.")
-
+if __name__ == '__main__':
+    main()
